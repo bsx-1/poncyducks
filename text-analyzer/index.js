e1f8ec2cb5c2c7e37b5e23abcd35642ae5b8acb1
NickDuggar <nbduggar@gmail.com>
2019-11-02 17:30:16 -0500

    Got example code to work

--- text-analyzer/index.js
+++ text-analyzer/index.js
@@ -12,3 +12,40 @@ if (!process.env[endpoint_var]) {
     throw new Error('please set/export the following environment variable: ' + endpoint_var);
 }
 const endpoint = process.env[endpoint_var];
+
+const creds = new CognitiveServicesCredentials.ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': subscription_key } });
+const client = new TextAnalyticsAPIClient.TextAnalyticsClient(creds, endpoint);
+
+// 0 is negative, 1 is positive
+/*
+const inputDocuments = {documents:[
+    {language:"en", id:"1", text:"I had the best day of my life."}
+]}
+
+const operation = client.sentiment({multiLanguageBatchInput: inputDocuments})
+operation
+.then(result => {
+    console.log(result.documents);
+})
+.catch(err => {
+    throw err;
+});*/
+
+// can use entity recognition for resume comparison!
+
+let inputLanguage = {
+    documents: [
+        {language:"en", id:"1", text:"My cat might need to see a veterinarian."}
+    ]
+};
+
+let operation = client.keyPhrases({
+    multiLanguageBatchInput: inputLanguage
+    });
+    operation
+    .then(result => {
+        console.log(result.documents);
+    })
+    .catch(err => {
+        throw err;
+});
