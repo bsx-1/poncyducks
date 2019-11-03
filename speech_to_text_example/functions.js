5c123704bb8c74fc290aae646ff583b9a13b2d44
Takuma <tfujiwara2017@gmail.com>
2019-11-03 08:06:13 -0600

    divided javascript into functions

--- speech_to_text_example/functions.js
+++ speech_to_text_example/functions.js
@@ -8,19 +8,18 @@ var authorizationToken;
 var SpeechSDK;
 var recognizer;
 var fillerWords = ["umm", "ummm", "um", "ah", "ahh", "ahhh", "uh", "uhh", "uhhh","so","like", "yeah",  "ok", "well", "right","see","hm","basically"];
+var myJsonRequest;
+var myFinalText;
 
 document.addEventListener("DOMContentLoaded", function () {
-  startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
+  init();
+  /*startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
   stopButton = document.getElementById("stopRecognition");
   subscriptionKey = document.getElementById("subscriptionKey");
   serviceRegion = document.getElementById("serviceRegion");
   phraseDiv = document.getElementById("phraseDiv");
 
-  //will hold JSON with all sentences by the time stop button pressed
-  var myJsonRequest = createInitialJson();
 
-  //will hold a string with all speech recognized by the time stop button is pressed
-  var myFinalText = "";
 
 
   stopButton.addEventListener("click", function(){
@@ -88,6 +87,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
   if (!!window.SpeechSDK) {
     SpeechSDK = window.SpeechSDK;
     startRecognizeOnceAsyncButton.disabled = false;
@@ -100,11 +100,107 @@ document.addEventListener("DOMContentLoaded", function () {
         RequestAuthorizationToken();
     }
   }
+  */
+
 });
 
 function test(){
   var testTxt = document.getElementById("testpara");
   console.log(findFillerWord(sentenceToHash("Hello, so like like so so so so I am")));
+}
+
+function init(){
+  phraseDiv = document.getElementById("phraseDiv");
+  startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
+  subscriptionKey = "f05deb49d4f943ec8fbe53adcf5bdfd9";
+  serviceRegion = "westus";
+  phraseDiv.innerHTML = "";
+  //will hold JSON with all sentences by the time stop button pressed
+  myJsonRequest = createInitialJson();
+
+  //will hold a string with all speech recognized by the time stop button is pressed
+  myFinalText = "";
+  if (!!window.SpeechSDK) {
+    SpeechSDK = window.SpeechSDK;
+    startRecognizeOnceAsyncButton.disabled = false;
+
+    document.getElementById('content').style.display = 'block';
+    document.getElementById('warning').style.display = 'none';
+
+    // in case we have a function for getting an authorization token, call it.
+    if (typeof RequestAuthorizationToken === "function") {
+        RequestAuthorizationToken();
+    }
+  }
+}
+
+function startSpeechToText(){
+    phraseDiv = document.getElementById("phraseDiv");
+    startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
+    subscriptionKey = "f05deb49d4f943ec8fbe53adcf5bdfd9";
+    serviceRegion = "westus";
+
+    startRecognizeOnceAsyncButton.disabled = true;
+    phraseDiv.innerHTML = "";
+
+    // if we got an authorization token, use the token. Otherwise use the provided subscription key
+    var speechConfig;
+    if (authorizationToken) {
+      speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(authorizationToken, serviceRegion.value);
+    } else {
+      if (subscriptionKey.value === "" || subscriptionKey.value === "subscription") {
+        alert("Please enter your Microsoft Cognitive Services Speech subscription key!");
+        return;
+      }
+      speechConfig = SpeechSDK.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
+    }
+
+    speechConfig.speechRecognitionLanguage = "en-US";
+    var audioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+    recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+
+
+    recognizer.startContinuousRecognitionAsync(
+      function () {
+        startRecognizeOnceAsyncButton.disabled = true;
+      },
+      function (err) {
+        startRecognizeOnceAsyncButton.disabled = false;
+        phraseDiv.innerHTML += err;
+        window.console.log(err);
+
+        recognizer.close();
+        recognizer = undefined;
+      });
+
+      //  The event recognizing signals that an intermediate recognition result is received.
+    recognizer.recognizing = function(s, e){
+        console.log('recognizing text', e.result.text);
+    };
+
+    //  The event recognized signals that a final recognition result is received.
+    recognizer.recognized = function(s, e){
+        phraseDiv.innerHTML += e.result.text;
+        myFinalText += e.result.text;
+        myJsonRequest = addSentenceToJson(e.result.text,myJsonRequest);
+        console.log('recognized text', e.result.text);
+    };
+}
+
+function stopSpeechToText(){
+  if(recognizer==undefined){
+
+    phraseDiv.innerHTML += "no recognizer";
+  } else {
+    var wordCounts = sentenceToHash(myFinalText);
+    console.log(wordCounts);
+    //displays word most frequent
+    console.log(sortedWordCount(wordCounts)[0][0]);
+    startRecognizeOnceAsyncButton.disabled = false;
+    phraseDiv.innerHTML += "stopped";
+    recognizer.close();
+    recognizer = undefined;
+  }
 }
 
 //sends JSON to sentiment analyzer.
