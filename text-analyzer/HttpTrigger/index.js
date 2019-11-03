b726ebaededd6409f7288bc260068a9dff5db156
NickDuggar <nbduggar@gmail.com>
2019-11-02 21:59:18 -0500

    Sentiment Analyzer works locally

--- text-analyzer/HttpTrigger/index.js
+++ text-analyzer/HttpTrigger/index.js
@@ -15,46 +15,34 @@ const endpoint = process.env[endpoint_var];
 
 const creds = new CognitiveServicesCredentials.ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': subscription_key } });
 const client = new TextAnalyticsAPIClient.TextAnalyticsClient(creds, endpoint);
-/*
-// 0 is negative, 1 is positive
 
-const inputDocuments = {documents:[
-    {language:"en", id:"1", text:"I had the best day of my life."}
-]}
-
-const operation = client.sentiment({multiLanguageBatchInput: inputDocuments})
-operation
-.then(result => {
-    console.log(result.documents);
-})
-.catch(err => {
-    throw err;
-});*/
+// 0 negative, 1 positive
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     console.log(req);
-    var inputDocuments = req.data;
+    var inputDocuments = req.body.data;
+    console.log(inputDocuments);
     var status;
-    var body;
-
-    if (inputDocuments) {
+    var myBody = "test1";
+    await client.sentiment({multiLanguageBatchInput: inputDocuments})
+    .then(result => {
         status = 200;
-        client.sentiment({multiLanguageBatchInput: inputDocuments})
-        .then(result => {
-             console.log(result.documents);
-             body = result.documents;
-        })
-        .catch(err => {
-            throw err;
-        });
-    }
-    else {
+        console.log(result.documents);
+        myBody = result.documents;
+        console.log(myBody);
+        context.res = {
+            status: status,
+            body: myBody
+        }
+    })
+    .catch(err => {
         status = 400;
-        body = 'Please pass data in the form {documents:[{language:""en"", id:""1"", text:""I had the best day of my life.""}]}';
-    }
-    context.res = {
-        status: status,
-        body: body
-    }
-};
+        myBody = 'Please pass data in the form {documents:[{language:""en"", id:""1"", text:""I had the best day of my life.""}]}';
+        context.res = {
+            status: status,
+            body: myBody
+        }
+        throw err;
+    });
+}
